89e10a3374f61eb7c7ba603a3a0e76474efe02b9
zhangxiaoxia <zhangxiaoxia@51talk.com>
2019-03-05 16:06:48 +0800

    构建组件模板

---
+++ docs/pages/project-features.vue
@@ -0,0 +1,179 @@
+<template lang="html">
+    <div id="project-features">
+        <h1 class="title">Vue Tags Input</h1>
+        <p>A generic UI component to input tags, with a couple of features</p>
+        <div class="features">
+            <vue-tags-input
+                    v-model="tag"
+                    class="tags-input"
+                    :tags="tags"
+                    :autocomplete-items="items"
+                    :allow-edit-tags="true"
+                    :placeholder="'Add a feature'"
+                    @tags-changed="newTags => tags = newTags"
+            />
+            <search v-model="searchText"></search>
+            <P>{{searchText}}</P>
+        </div>
+    </div>
+</template>
+
+<script>
+    import VueTagsInput from '@johmun/vue-tags-input';
+    import search from '../../search/index';
+
+    export default {
+        name: 'ProjectFeatures',
+        components: {
+            VueTagsInput,
+            search
+        },
+        data() {
+            return {
+                tag: '',
+                tags: [],
+                searchText: '',
+                features: [
+                    'No dependencies',
+                    'Custom validation rules',
+                    'Hooks: Before adding, Before deleting ...',
+                    'Edit tags after creation (click me)',
+                    'Fast setup',
+                    'Works with Vuex',
+                    'Small size: 34kb minified (css included) | gzipped 9kb',
+                    'Autocompletion',
+                    'Customization options',
+                    'Templating',
+                    'Delete tags on backspace',
+                    'Add tags on paste',
+                    'Examples & Docs',
+                    'More ...',
+                ],
+                todo: [
+                    'Animation support',
+                    'Autocomplete Highlighting',
+                    'Groups',
+                ],
+            };
+        },
+        computed: {
+            items() {
+                return this.todo
+                    .filter(t => new RegExp(this.tag, 'i').test(t))
+                    .map(i => {
+                        return {text: `Todo: ${i}`};
+                    });
+            },
+        },
+        created() {
+            this.tags = this.features.map(text => ({text}));
+        },
+    };
+</script>
+
+<style lang="scss">
+    @import '~colors';
+
+    #app #project-features .tags-input {
+        width: 100%;
+        max-width: none;
+
+        .ti-input {
+            border: none;
+            border-bottom: 1px solid $lightGrey;
+            padding: 4px 12px;
+        }
+
+        .ti-tag input {
+            color: $darker;
+        }
+
+        .ti-tag, .ti-item.ti-selected-item {
+            background-color: $primary;
+            color: $darker;
+        }
+
+        .ti-tags li {
+            padding: 5px 8px;
+            margin: 2px;
+        }
+
+        .ti-tag.dark-font {
+            color: #333;
+        }
+
+        .ti-item {
+            padding: 2px 3px;
+        }
+
+        .ti-tag.ti-deletion-mark {
+            background-color: $error !important;
+            color: #fff;
+        }
+    }
+
+    #project-features .browser-support .icon {
+        width: 100%;
+        height: auto;
+        padding: 16px;
+
+        > * {
+            fill: #b7c4c9;
+        }
+    }
+</style>
+
+<style lang="scss" scoped>
+    @import 'colors';
+
+    .title {
+        line-height: 28px;
+        font-size: 23px;
+        font-weight: bold;
+        font-family: 'Raleway', sans-serif;
+        color: #fff;
+        margin-bottom: 20px
+    }
+
+    .features {
+        display: flex;
+        margin-bottom: 40px;
+        flex-direction: column;
+    }
+
+    .browser-support {
+        display: flex;
+        flex-wrap: wrap;
+
+        > div {
+            display: flex;
+            flex-direction: column;
+            flex: 1;
+            align-items: center;
+            font-size: 14px;
+            min-width: 85px;
+            margin: 8px;
+            max-width: 94px;
+        }
+
+        span {
+            display: flex;
+            margin-top: 0px;
+            font-weight: 500;
+
+            i {
+                margin-left: 3px;
+                font-size: 20px;
+            }
+        }
+    }
+
+    .license {
+        margin-top: 40px;
+        font-size: 0.85em;
+    }
+
+    .date {
+        font-size: 0.85em;
+    }
+</style>
---
+++ search/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="search-field flex-box">
+        <i class="search-icon"></i>
+        <div class="search-input">
+            <input type="search" @input="$emit('input', $event.target.value)"  :placeholder="placeholder" class="input-field"/>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            placeholder: {
+                default: '请输入查询信息',
+                type: String
+            },
+            value: {}
+        }
+    };
+</script>
+<style lang="less" scoped>
+    .flex-box {
+        display: flex;
+    }
+
+    .search-field {
+        width: 100%;
+        background: rgba(242, 242, 242, 1);
+        border-radius: 34px;
+        padding: 4px 8px;
+        box-sizing: border-box;
+
+        .search-icon {
+            display: inline-block;
+            width: 24px;
+            height: 24px;
+            background: url("./img/search.png") no-repeat;
+            background-size: 100% 100%;
+        }
+        .search-input{
+            flex: 1;
+        }
+
+        .input-field {
+            width: 100%;
+            font-size: 14px;
+            font-weight: 400;
+            color: rgba(153, 153, 153, 1);
+            border: 0;
+            background: transparent;
+            box-sizing: border-box;
+        }
+    }
+</style>
